7e7611403f6f4e9202effb87a0a0d58f3e0b5439
Jake Whitton <jwhitton@mit.edu>
2019-09-14 20:03:04 -0400

    Make new game logic compile

--- src/character.h
+++ src/character.h
@@ -2,9 +2,20 @@ struct Character
 {
 	int location;
 	int hp;
+
+    Character(int locationValue, int hpValue)
+        : location(locationValue)
+        , hp(hpValue)
+    { }
 };
 
 struct Player : public Character {
 	bool isRechargeMode;
 	int attackLockOut;
+
+    Player (int location, int hp, bool isRechargeModeValue, int attackLockOutValue)
+        : Character(location, hp)
+        , isRechargeMode(isRechargeModeValue)
+        , attackLockOut(attackLockOutValue)
+    { }
 };
--- src/gamelogic.cpp
+++ src/gamelogic.cpp
@@ -29,123 +29,6 @@ static constexpr int PLAYER_HEALTH_LOSS = 26;
 static constexpr int ENEMY_HEALTH_LOSS = 52;
 static constexpr int RECHARGE_AMOUNT = 26;
 
-
-Frame makeFrame(InputState input, Player *player, std::vector<Character> enemies)
-{
-	bool endGame = false;
-	bool dead = false;
-    
-	
-	//player movement
-    if (input[RIGHT].isHeld)
-    {
-        currCol += 1;
-        
-    }
-    else if(input[LEFT].isHeld && currCol > 0)
-    {
-        currCol -= 1;
-    }
-
-	//fighting
-	bool playerAttacking = false;
-	if (input[ONE].isPressed)
-	{
-		//attack if in normal mode
-		if (!player.isRechargeMode)
-		{
-			//attack
-			if (player.attackLockOut == 0)
-			{
-				playerAttacking = true;
-				player.attackLockOut = MAX_ATTACK_LOCKOUT;
-			}
-		}
-	}
-	bool playerHit = false;
-	int numEnemies = enemies.size();
-	for (int i = 0; i < enemies.size(); i++)
-	{
-		if (enemies[i].location == player->location) 
-		{
-			if (!player->isRechargeMode && playerAttacking) 
-			{
-				enemies[i].hp -= ENEMY_HEALTH_LOSS;
-				if (enemies[i].hp <= 0) 
-				{
-					enemies.erase(enemies.begin() + i);
-					numEnemies -= 1;
-					i -= 1;
-				}
-			}
-			else if (!player->isRechargeMode && !playerAttacking) 
-			{
-				player.hp -= PLAYER_HEALTH_LOSS;
-				playerHit = true;
-			}
-			else if (player->isRechargeMode) 
-			{
-				player.hp = 0;
-				endGame = true;
-				dead = true;
-			}
-			player->location -= BOUNCE_DISTANCE;
-			enemies[i].location += BOUNCE_DISTANCE;
-		}
-	}
-
-	boolean rechargingAllowed = true;
-	//switch recharge mode
-    if (input[TWO].isPressed)
-    {
-		player->isRechargeMode = !player->isRechargeMode;
-		rechargingAllowed = false;
-    }
-    
-	//player recharging
-	if (input[B].isPressed && player->isRechargeMode && rechargingAllowed) 
-	{
-		player->hp += RECHARGE_AMOUNT;
-	}
-
-	//enemy movement
-	for (int i = 0; i < enemies.size(); i++) 
-	{
-		int enemyMovement = (rand() % 2) * 2 - 1;
-		enemies[i].location += enemyMovement;
-	}
-	
-	//enemy creation
-	bool createEnemy = (rand() % 100) < ENEMY_PROBABILITY_PERCENT;
-	if (createEnemy && enemies.size() < MAX_ENEMY_COUNT) 
-	{
-		int startLocation = player->location + 1 + (rand() % ENEMY_DISTANCE_RANGE);
-		Character enemy{ startLocation, ENEMY_START_HP };
-		enemies.push_back(enemy);
-	}
-
-	if (player->location == cols - 1) 
-	{
-		endGame = true;
-		dead = false;
-	}
-
-
-    return frameFromPixels(&player, enemies, endGame, dead, playerHit); 
-}
-
-static map<int, const char *> buttonNames
-{
-    {UP, "UP"},
-    {RIGHT, "RIGHT"},
-    {DOWN, "DOWN"},
-    {LEFT, "LEFT"},
-    {A, "A"},
-    {B, "B"},
-    {ONE, "ONE"},
-    {TWO, "TWO"}
-};
-
 Frame frameFromPixels(Player *player, std::vector<Character> enemies, bool endGame, bool dead, bool playerHit) {
 	Frame frame;
 	memset(frame.data(), 0, sizeof frame);
@@ -190,6 +73,123 @@ Frame frameFromPixels(Player *player, std::vector<Character> enemies, bool endGa
 	return frame;
 }
 
+Frame makeFrame(InputState input, Player *player, std::vector<Character> & enemies)
+{
+	bool endGame = false;
+	bool dead = false;
+    
+	
+	//player movement
+    if (input[RIGHT].isHeld)
+    {
+        currCol += 1;
+        
+    }
+    else if(input[LEFT].isHeld && currCol > 0)
+    {
+        currCol -= 1;
+    }
+
+	//fighting
+	bool playerAttacking = false;
+	if (input[ONE].isPressed)
+	{
+		//attack if in normal mode
+		if (!player->isRechargeMode)
+		{
+			//attack
+			if (player->attackLockOut == 0)
+			{
+				playerAttacking = true;
+				player->attackLockOut = MAX_ATTACK_LOCKOUT;
+			}
+		}
+	}
+	bool playerHit = false;
+	int numEnemies = enemies.size();
+	for (int i = 0; i < enemies.size(); i++)
+	{
+		if (enemies[i].location == player->location) 
+		{
+			if (!player->isRechargeMode && playerAttacking) 
+			{
+				enemies[i].hp -= ENEMY_HEALTH_LOSS;
+				if (enemies[i].hp <= 0) 
+				{
+					enemies.erase(enemies.begin() + i);
+					numEnemies -= 1;
+					i -= 1;
+				}
+			}
+			else if (!player->isRechargeMode && !playerAttacking) 
+			{
+				player->hp -= PLAYER_HEALTH_LOSS;
+				playerHit = true;
+			}
+			else if (player->isRechargeMode) 
+			{
+				player->hp = 0;
+				endGame = true;
+				dead = true;
+			}
+			player->location -= BOUNCE_DISTANCE;
+			enemies[i].location += BOUNCE_DISTANCE;
+		}
+	}
+
+	bool rechargingAllowed = true;
+	//switch recharge mode
+    if (input[TWO].isPressed)
+    {
+		player->isRechargeMode = !player->isRechargeMode;
+		rechargingAllowed = false;
+    }
+    
+	//player recharging
+	if (input[B].isPressed && player->isRechargeMode && rechargingAllowed) 
+	{
+		player->hp += RECHARGE_AMOUNT;
+	}
+
+	//enemy movement
+	for (int i = 0; i < enemies.size(); i++) 
+	{
+		int enemyMovement = (rand() % 2) * 2 - 1;
+		enemies[i].location += enemyMovement;
+	}
+	
+	//enemy creation
+	bool createEnemy = (rand() % 100) < ENEMY_PROBABILITY_PERCENT;
+	if (createEnemy && enemies.size() < MAX_ENEMY_COUNT) 
+	{
+		int startLocation = player->location + 1 + (rand() % ENEMY_DISTANCE_RANGE);
+		Character enemy{ startLocation, ENEMY_START_HP };
+		enemies.push_back(enemy);
+	}
+
+	if (player->location == cols - 1) 
+	{
+		endGame = true;
+		dead = false;
+	}
+
+
+    return frameFromPixels(player, enemies, endGame, dead, playerHit); 
+}
+
+static map<int, const char *> buttonNames
+{
+    {UP, "UP"},
+    {RIGHT, "RIGHT"},
+    {DOWN, "DOWN"},
+    {LEFT, "LEFT"},
+    {A, "A"},
+    {B, "B"},
+    {ONE, "ONE"},
+    {TWO, "TWO"}
+};
+
+
 void doOneFrame(Player *player, std::vector<Character> enemies) 
 {
 	InputState input = getButtonStates();
@@ -204,7 +204,7 @@ void doOneFrame(Player *player, std::vector<Character> enemies)
 
     printf("\n");
 
-	Frame frame = makeFrame(input);
+	Frame frame = makeFrame(input, player, enemies);
     renderFrame(frame);
 }
 
@@ -217,7 +217,7 @@ void newGame()
 	while (true) 
 	{
 		doOneFrame(&player, enemies);
-		int sleepTimeInMilli = 100;
-		usleep(sleepTimeInMilli); //so that frames only update once a second
+		int sleepTimeInMilli = 1000;
+		usleep(sleepTimeInMilli * 1000); //so that frames only update once a second
 	}
 }
